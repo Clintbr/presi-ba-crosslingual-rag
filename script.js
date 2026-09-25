3cb59c0918fa71953ac494b6263a7a2e2b197e6b
Clint Bryan Nguena <clint.bryan.nguena@mni.thm.de>
2026-09-25 15:59:39 +0200

    feat: slides with play mode

--- script.js
+++ script.js
@@ -107,7 +107,7 @@ const plans = [
   } },
   { duration: 45000, prepare(slide, d) {
     const list = q(slide, ".agenda-list"), items = q(slide, ".agenda-list p");
-    hide([...list, ...items]); later(() => reveal(list), 10000); sequence(items, 15000, 5000, d, 5000);
+    hide([...list, ...items]); later(() => reveal(list), 10000); sequence(items, 10000, 5000, d, 5000);
   } },
   { duration: 225000, prepare(slide) {
     const cards = q(slide, ".lang-card"), arrows = q(slide, ".language-flow .arrow"), panels = q(slide, ".research-comparison > div"), ref = q(slide, ".research-comparison > div:first-child li"), own = q(slide, ".research-comparison > div:last-child li");
